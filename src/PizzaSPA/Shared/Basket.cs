8c837f0e24e76957ca3e51159ac28ac7a9722b3b
Aleksey Vasilyev <vasiljev.alexey@otr.ru>
2019-09-20 14:09:01 +0300

    Закончена глава 3

--- src/PizzaSPA/Shared/Basket.cs
+++ src/PizzaSPA/Shared/Basket.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections.Generic;
-
-namespace PizzaSPA.Shared
+﻿namespace PizzaSPA.Shared
 {
+    using System.Collections.Generic;
+    /// <summary>
+    /// Корзина
+    /// </summary>
     public class Basket
+    
     {
+        /// <summary>
+        /// Заказчик
+        /// </summary>
         public Customer Customer { get; set; } = new Customer();
 
+        /// <summary>
+        /// Заказы
+        /// </summary>
         public List<int> Orders { get; set; } = new List<int>();
 
+        /// <summary>
+        /// Заказ оплачен
+        /// </summary>
         public bool HasPaid { get; set; } = false;
 
+        /// <summary>
+        /// Добавить пиццу
+        /// </summary>
+        /// <param name="pizzaId">Иднетификтор пиццы</param>
         public void Add(int pizzaId)
         {
-             Orders.Add(pizzaId);
+            Orders.Add(pizzaId);
         }
-
+        /// <summary>
+        /// Удалить пиццу из корзины
+        /// </summary>
+        /// <param name="pos">Иднетификтор пиццы</param>
         public void Remove(int pos)
         {
             Orders.RemoveAt(pos);
